f5ee66be8eff1aea4f27bd413ed3b7e2bce9bea1
shafek <engahmedshafek1@gmail.com>
2024-10-23 19:54:13 +0300

    first commit

---
+++ src/api/clinc/controllers/clinc.ts
@@ -0,0 +1,7 @@
+/**
+ * clinc controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::clinc.clinc');
---
+++ src/api/clinc/routes/clinc.ts
@@ -0,0 +1,7 @@
+/**
+ * clinc router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::clinc.clinc');
